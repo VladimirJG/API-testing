7b8871cf6cd04b8e61c8c81879af0ac0ec678635
VladimirJG <vladimi85@gmail.com>
2023-03-27 11:34:10 +0300

    all avatar modified, check users id

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -5,12 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class ReqresTest {
     private final static String URL = "https://reqres.in/";
+    private final static List<String> list = new ArrayList<>();
 
     @Test
     public void getAllUsers() {
@@ -30,7 +32,19 @@ public class ReqresTest {
                 .get(URL + "api/users")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
-        allUsers.stream().map(UserData::getAvatar).forEach(System.out::println);
+        list.addAll(allUsers.stream().map(UserData::getAvatar).toList());
+        System.out.println(list);
     }
 
+    @Test
+    public void checkIdUsers() {
+        List<UserData> allUsers = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL + "api/users")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+        allUsers.forEach(u -> Assert.assertTrue(u.getAvatar().contains(u.getId().toString())));
+
+    }
 }
